f3b23e813dddb519386bdadd7aa4ebfada3c1596
Philipp Möller <bootsarehax@googlemail.com>
2012-02-18 19:46:58 +0100

    added dired+

--- init.el
+++ init.el
@@ -42,9 +42,15 @@
 
 (let ((default-directory  "~/.emacs.d/"))
   (normal-top-level-add-to-load-path 
-   '("codepad" "keyfreq" "mediawiki" "emacs-color-theme-solarized" "autopair" "expand-region.el")))
+   '("codepad" "keyfreq" "mediawiki" "emacs-color-theme-solarized" "autopair" "expand-region.el" "dired-plus")))
 
 (load-file "~/.emacs.d/zenburn/color-theme-zenburn.el")
+
+;;
+;; dired-plus
+;; 
+
+(require 'dired+)
 
 ;; ask briefly
 (defalias 'yes-or-no-p 'y-or-n-p)
