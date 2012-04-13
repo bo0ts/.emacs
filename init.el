312d3f195892a2540106ef8dff075aab761de1a5
Philipp Möller <bootsarehax@googlemail.com>
2012-04-13 17:52:43 +0200

    get rid of broken latex subscripts.

--- init.el
+++ init.el
@@ -35,6 +35,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(font-latex-subscript-face ((t nil)))
  '(org-hide ((((background dark)) (:foreground "#2b2b2b")))))
 
 ;;
