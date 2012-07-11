64f10a640353ddf65c0c0fe9bc228ef071552594
Philipp Möller <bootsarehax@googlemail.com>
2012-07-11 19:41:03 +0200

    Remove old urgency hints for erc

--- init.el
+++ init.el
@@ -295,27 +295,6 @@
 
 (setq erc-auto-query 'buffer)
 
-(defun x-urgency-hint (frame arg &optional source)
-  (let* ((wm-hints (append (x-window-property 
-			    "WM_HINTS" frame "WM_HINTS" 
-			    (if source
-				source
-			      (string-to-number 
-			       (frame-parameter frame 'outer-window-id)))
-			    nil t) nil))
-	 (flags (car wm-hints)))
-    (setcar wm-hints
-	    (if arg
-		(logior flags #x00000100)
-	      (logand flags #xFFFFFEFF)))
-    (x-change-window-property "WM_HINTS" wm-hints frame "WM_HINTS" 32 t)))
-
-(defadvice erc-track-find-face (around erc-track-find-face-promote-query activate)
-  (if (erc-query-buffer-p) 
-      (x-urgency-hint (selected-frame) t)
-    ;;(setq ad-return-value (intern "erc-current-nick-face")))
-    ad-do-it))
-
 (setq erc-autojoin-channels-alist
       '(("freenode.net" "#emacs" "#archlinux" "#haskell" "#xmonad" "##c++")))
 
