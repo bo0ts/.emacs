8aec97da53c4237427c2ee70310661c535fdaddc
Philipp Möller <bootsarehax@googlemail.com>
2012-09-24 17:07:36 +0200

    Remove bbdb-org compat workaround as it is no longer necessary.
    I have no idea if this is triggered by org-mode 7.9 or a new bbdb
    version.

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(calendar-week-start-day 1)
- '(custom-safe-themes (quote ("e1b2af501dbee3c915fe3940defe66952044f795e47821ff5796054024596408" "7cd5b0111f13d54f72946ca9f4d8e0bc488f305308d6faa6f165fba138e21247" "08cb00e329be5e3186826ae7e1a84cf526fe2a2784c048c8ae8d5023877ccbe5" "bf9d5728e674bde6a112979bd830cc90327850aaaf2e6f3cc4654f077146b406" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
+ '(custom-safe-themes (quote ("78b1c94c1298bbe80ae7f49286e720be25665dca4b89aea16c60dacccfbb0bca" "e1b2af501dbee3c915fe3940defe66952044f795e47821ff5796054024596408" "7cd5b0111f13d54f72946ca9f4d8e0bc488f305308d6faa6f165fba138e21247" "08cb00e329be5e3186826ae7e1a84cf526fe2a2784c048c8ae8d5023877ccbe5" "bf9d5728e674bde6a112979bd830cc90327850aaaf2e6f3cc4654f077146b406" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
  '(doxymacs-doxygen-style "JavaDoc")
  '(erc-modules (quote (autojoin button completion fill irccontrols keep-place list match menu move-to-prompt netsplit networks noncommands readonly ring stamp spelling track)))
  '(erc-nickserv-identify-mode (quote nick-change))
@@ -475,28 +475,9 @@
       bbdb-complete-mail-allow-cycling t
       bbdb-phone-style nil
       org-bbdb-anniversary-field 'birthday)
+
 (bbdb-initialize 'gnus 'message)
 (bbdb-mua-auto-update-init 'gnus 'message)
-
-(defalias 'bbdb-company 'bbdb-search-organization)
-(defalias 'bbdb-name 'bbdb-search-name)
-
-(defun bbdb-record-getprop (record label)
-  (and (eq label 'company)
-       (setq label 'organization))
-  (if (memq label '(name degree organization address phone mail aka))
-      (funcall
-       (intern
-        (concat "bbdb-record-" (symbol-name label)))
-       record)
-    (bbdb-record-note record label)))
-
-(defadvice bbdb-split (around my-bbdb-split activate)
-  (when (or (string= string "\n") (string= string "-"))
-    (let ((sep string))
-      (setq string separator
-            separator sep)
-      ad-do-it)))
 
 ;;
 ;; ispell dictionaries and flyspell
