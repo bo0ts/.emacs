797839ce9d146ef65be8a9f688a3187601431497
Philipp Möller <bootsarehax@gmail.com>
2015-05-27 12:37:56 +0200

    Some dired fine-tuning

--- init.el
+++ init.el
@@ -160,6 +160,10 @@
 ;; 
 
 (require 'dired+)
+(require 'dired-x)
+(setq-default dired-omit-files-p t) ; Buffer-local variable
+(setq dired-omit-files (concat dired-omit-files "\\|^\\..+$")
+      dired-listing-switches "-aBhl  --group-directories-first")
 
 ;;
 ;; version-control
