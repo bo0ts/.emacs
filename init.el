7196e6f502cc36b4a543beab5e425b1b48e1c98e
Philipp Möller <bootsarehax@gmail.com>
2015-07-30 14:35:59 +0200

    txt overwrites CMakeLists.txt and I use it more

--- init.el
+++ init.el
@@ -194,7 +194,6 @@
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.txt" . markdown-mode))
 
 ;; Hacky, but solid for ArchLinux.
 (setq markdown-command "perl /usr/bin/vendor_perl/Markdown.pl")
